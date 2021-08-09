a6f5a67a1d9eaa4046d075c1417f5c042dd30c38
Miguel Grinberg <miguel.grinberg@gmail.com>
2021-08-09 23:58:03 +0100

    Additional work on the Flask-Login async patch

--- src/aioflask/patched/flask_login/__init__.py
+++ src/aioflask/patched/flask_login/__init__.py
@@ -1,10 +1,12 @@
 from functools import wraps
+import sys
 from werkzeug.local import LocalProxy
 from aioflask import current_app, g
 from flask import _request_ctx_stack
 from aioflask.patch import patch_decorator, patch_decorator_method
 import flask_login
-from flask_login import login_required, fresh_login_required, LoginManager
+from flask_login import login_required, fresh_login_required, \
+    LoginManager as OriginalLoginManager
 
 for symbol in flask_login.__all__:
     try:
@@ -12,54 +14,67 @@ for symbol in flask_login.__all__:
     except AttributeError:
         pass
 
-# the decorators that register callbacks need to be patched to support async
-# functions
-LoginManager.user_loader = patch_decorator_method(
-    LoginManager, 'user_loader')
-LoginManager.header_loader = patch_decorator_method(
-    LoginManager, 'header_loader')
-LoginManager.request_loader = patch_decorator_method(
-    LoginManager, 'request_loader')
-LoginManager.unauthorized_handler = patch_decorator_method(
-    LoginManager, 'unauthorized_handler')
-LoginManager.needs_refresh_handler = patch_decorator_method(
-    LoginManager, 'needs_refresh_handler')
 
-# The current_user context local is implemented in a way that creates some
-# complications for aioflask:
-# 1. It can cause the user loader callback to be called at a random time in the
-#    life of the request. Since the user loader can now be async, it might need
-#    to be awaited, but doing (await current_user) would be really weird.
-# 2. It caches the retrieved user in a custom property of the request context,
-#    which Flask ignores when making request context copies.
-#
-# To address #1, the login_requred and fresh_login_required are modified to
-# load the user and cache it, before the view function is called.
-# To address #2, the current_user context local is modified to cache the user
-# in the g object.
-
-original_login_required = patch_decorator(login_required)
-original_fresh_login_required = patch_decorator(fresh_login_required)
+def _user_context_processor():
+    return {'current_user': _get_user()}
 
 
-def login_required(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        current_app.login_manager._load_user()
-        g.flask_login_current_user = _request_ctx_stack.top.user
-        return original_login_required(f)(*args, **kwargs)
-
-    return wrapper
-
-
-def fresh_login_required(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        LoginManager._load_user()
-        g.flask_login_current_user = current_user._get_current_object()
-        return original_fresh_login_required(f)(*args, **kwargs)
-
-    return wrapper
+def _load_user():
+    # Obtain the current user and preserve it in the g object. Flask-Login
+    # saves the user in a custom attribute of the request context, but that
+    # doesn't work with aioflask because when a copy of the request context is
+    # made, custom attributes are not carried over to the copy.
+    current_app.login_manager._load_user()
+    g.flask_login_current_user = _request_ctx_stack.top.user
 
 
-current_user = LocalProxy(lambda: g.flask_login_current_user)
+def _get_user():
+    # Return the current user. This function is linked to the current_user
+    # context local, but unlike the original in Flask-Login, it does not
+    # attempt to load the user, it just returns the user that was pre-loaded.
+    # This avoids the somewhat tricky complication of triggering database
+    # operations that need to be awaited, which would require using something
+    # like (await current_user)
+    if hasattr(g, 'flask_login_current_user'):
+        return g.flask_login_current_user
+    return current_app.login_manager.anonymous_user()
+
+
+class LoginManager(OriginalLoginManager):
+    def init_app(self, app, add_context_processor=True):
+        super().init_app(app, add_context_processor=False)
+        if add_context_processor:
+            app.context_processor(_user_context_processor)
+
+        # To prevent the current_user context local from triggering I/O at a
+        # random time when it is first referenced (which is a big complication
+        # if the I/O is async and needs to be awaited), we force the user to be
+        # loaded before each request. This isn't a perfect solution, because
+        # a before request handler registered before this one will not see the
+        # current user.
+        app.before_request(_load_user)
+
+    # the decorators that register callbacks need to be patched to support
+    # async views
+    user_loader = patch_decorator_method(OriginalLoginManager, 'user_loader')
+    header_loader = patch_decorator_method(
+        OriginalLoginManager, 'header_loader')
+    request_loader = patch_decorator_method(
+        OriginalLoginManager, 'request_loader')
+    unauthorized_handler = patch_decorator_method(
+        OriginalLoginManager, 'unauthorized_handler')
+    needs_refresh_handler = patch_decorator_method(
+        OriginalLoginManager, 'needs_refresh_handler')
+
+
+# patch the two login_required decorators so that they accept async views
+login_required = patch_decorator(login_required)
+fresh_login_required = patch_decorator(fresh_login_required)
+
+
+# redefine the current_user context local
+current_user = LocalProxy(_get_user)
+
+# patch the _get_user() function in the flask_login.utils module so that any
+# calls to get current_user in Flask-Login functions are redirected here
+setattr(sys.modules['flask_login.utils'], '_get_user', _get_user)
